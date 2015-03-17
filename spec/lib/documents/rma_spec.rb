5aaa6208e59cfa777cf5217d2f33f9f25ef2e241
Jordan Brough <git.j@brgh.net>
2015-03-17 14:27:22 -0600

    Fix broken specs

--- spec/lib/documents/rma_spec.rb
+++ spec/lib/documents/rma_spec.rb
@@ -5,14 +5,14 @@ module Documents
 
     describe '#to_xml' do
       it 'should convert to rma document' do
-        shipment = Factories.shipment['shipment']
+        shipment = Factories.shipment
         doc = RMA.new(shipment, {})
         xml = Nokogiri::XML(doc.to_xml)
         xsd = Nokogiri::XML::Schema(File.read('./spec/schemas/rma.xsd'))
 
         errors = xsd.validate(xml).collect { |error| error }
 
-        expect(doc.name).to match "RMA__20140423"
+        expect(doc.name).to match /^RMA_#{shipment['id']}_/
         expect(errors).to eq []
       end
     end
