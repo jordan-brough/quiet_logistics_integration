33ae800aecc171a11021c57474db4f0e2ea86cd2
Jordan Brough <git.j@brgh.net>
2015-03-17 23:10:56 -0600

    Remove unused code

--- lib/documents/shipment_order_result.rb
+++ lib/documents/shipment_order_result.rb
@@ -30,8 +30,6 @@ module Documents
       @doc = Nokogiri::XML(xml)
       @shipment_number = @doc.xpath("//@OrderNumber").first.text
       @date_shipped = @doc.xpath("//@DateShipped").first.text
-      @freight_cost = @doc.xpath("//@FreightCost").first.text
-      @carton_count = @doc.xpath("//@CartonCount").first.text
       @tracking_number = @doc.xpath('//@TrackingId').first.value
       @business_unit = @doc.xpath('//@BusinessUnit').first.value
       @warehouse = @doc.xpath("//@Warehouse").first.text
